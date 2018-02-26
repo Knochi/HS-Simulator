2694e3a8b1ddb6f368e9c1d6f829060dc8e5d50f
Knochi <mail@knochi.de>
2018-02-26 16:06:53 +0100

    Add files via upload

--- p5/HS-Simulator/sketch.js
+++ p5/HS-Simulator/sketch.js
@@ -1,20 +1,27 @@
-var x=0;
 
 function setup() {
   // put setup code here
-  createCanvas(600,400);
-  line(15,25,70,90);
+  createCanvas(1024 ,768);
+  background(0);
+  sulaco=new battleShip(2);
+  sulaco.weapons[0]=new battery(5);
+  sulaco.display(40,40);
+  //textSize(32);
+  //noStroke();
+
 }
 
 function draw() {
   // put drawing code here
-  ellipse(x, height/2, 20, 20);
-  x = x+1;
+  fill(255);
+ text(sulaco.weapons[0].dps,80,80);
+ ellipse(130,130,80);
 }
 
 //battleShip class
 function battleShip(lvl) {
   var strengths=[]
+  img = loadImage("assets/BS_lvl3.png");
   this.level=lvl;
   this.hullStrength=500;
   this.speed=600;
@@ -23,9 +30,14 @@ function battleShip(lvl) {
   this.weaponSlots=1;
   this.shieldSlots=1;
   this.supportSlots=0;
+  this.weapons=[];
 
-  
-};
+  this.display = function(x, y){
+    image(img,x,y);
+  }
+
+
+}
 
 function battery(lvl){
   var dpsAr = [0,100,120,140,160,180,210,250,285,315,340];
@@ -34,5 +46,5 @@ function battery(lvl){
   this.dps=dpsAr[lvl]; //damage per second
   this.dps.ws=dpsWsAr[lvl]; //damage per hour in WS
   this.range=120; //range in AU
-
+  this.name="Battery";
 }
